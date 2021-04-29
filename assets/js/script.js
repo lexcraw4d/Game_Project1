1f59d6cf73c623c82c0af96b50d6161ef5011452
Lexie Crawford <lexcraw4d@gmail.com>
2021-04-29 00:56:36 -0400

    made rough outline to work with on select option values to POST to fetch url in future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
+//API Calls to rawg.io
+let genres = $("#genres :selected").val();
+let platforms = $("#console :selected").val();
+let rating = $("#rating :selected").val();
+
 
 
 
 function getGames(){
-    fetch ('https://api.rawg.io/api/genres'+ '?key=f4d775e86a54401da676771c802f8d33').then(res=>res.json()).then(data=>console.log(data))
+    fetch (`https://api.rawg.io/api/games?key=f4d775e86a54401da676771c802f8d33&ordering=-metacritic&genres=${genres}&platforms=${platforms}&rating=${rating}`).then(res=>res.json()).then(data=>console.log(data))
     }
     getGames();
-    
-    
+
+
+  //To Do: Learn PHP to self call to return variables
