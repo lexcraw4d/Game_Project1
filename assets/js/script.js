41c71dad030b0fe251f9aed4e7fddfde542fa73d
Lexie Crawford <lexcraw4d@gmail.com>
2021-04-30 02:57:25 -0400

    API calls user selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 //API Calls to rawg.io
-let genres = $("#genres :selected").val();
-let platforms = $("#console :selected").val();
-let rating = $("#rating :selected").val();
 
-
-
-
-function getGames(){
-    fetch (`https://api.rawg.io/api/games?key=f4d775e86a54401da676771c802f8d33&ordering=-metacritic&genres=${genres}&platforms=${platforms}&rating=${rating}`).then(res=>res.json()).then(data=>console.log(data))
-    }
-    getGames();
-
-
-  //To Do: Learn PHP to self call to return variables
+const button = document.getElementById('submit');
+button.addEventListener('click', function (e) {
+	let genres = document.getElementById('genres').value;
+	let rating = document.getElementById('rating').value;
+	let platforms = document.getElementById('console').value;
+	//console.log(genres);
+    let 
+	fetch(
+		`https://api.rawg.io/api/games?key=f4d775e86a54401da676771c802f8d33&ordering=-metacritic&genres=${genres}&platforms=${platforms}&rating=${rating}`
+	)
+		.then((res) => res.json())
+		.then((data) => console.log(data));
+});
