6217914e135e3d28dd07d4a48af7219382d4f213
Lexie Crawford <lexcraw4d@gmail.com>
2021-04-30 18:34:21 -0400

    added current game and console with line break

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,8 @@ let rating = document.getElementById('rating').value;
 let platforms = document.getElementById('console').value;
 let test = document.querySelector('#test');
 let divEl = document.createElement('div');
-let divElTwo = document.createElement('div');
+// let consoleEl =document.createElement('br');
+
 divEl.setAttribute('class', 'democlass');
 
 //console.log(genres);
@@ -17,18 +18,33 @@ function getGames() {
 	)
 		.then((res) => res.json())
 		.then((data) => {
-			for (let i = 0; i < data.results.length; i++) {
+			function loopAgain() {for (let i = 0; i < data.results.length; i++) {
 				let results = data.results[i];
 				test.append(divEl);
-				divEl.append(results.name);
-				for (let p = 0; p < data.results[i].platforms.length; p++) {
-					divEl.append(results.platforms[p].platform.name);
+				divEl.append(results.name)
+                divEl.innerHTML += '<br>'
+                console(results)
+                background(results)
+            }
+            function console(currentGame){
+				for (let p = 0; p < currentGame.platforms.length; p++) {
+                    divEl.append(currentGame.platforms[p].platform.name);
+                    divEl.innerHTML += '<br>'
 				}
-			}
+            }
+            function background(image){
+                for (let b=0; b<image.length; b++){
+                    var images = document.createElement('img');
+                    images.setAttribute('src',image.background_image)
+                    divEl.append(img)
+                }
+            }
+        }
+            loopAgain()
 		});
 }
 
-//ended loop through 2nd platform array need to create
+//ended loop through 2nd platform array need to create on HTML telement --- maybe in future dynamically through the DOM
 
 button.addEventListener('click', function (e) {
 	e.preventDefault();
