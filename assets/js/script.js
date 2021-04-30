8b21593afa50ab0c1769c6e8e33d8dba514e04f9
Lexie Crawford <lexcraw4d@gmail.com>
2021-04-30 02:57:51 -0400

    API calls user selection and appends Game name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,29 @@
 //API Calls to rawg.io
 
 const button = document.getElementById('submit');
-button.addEventListener('click', function (e) {
-	let genres = document.getElementById('genres').value;
-	let rating = document.getElementById('rating').value;
-	let platforms = document.getElementById('console').value;
-	//console.log(genres);
-    let 
+let genres = document.getElementById('genres').value;
+let rating = document.getElementById('rating').value;
+let platforms = document.getElementById('console').value;
+let test = document.querySelector('#test');
+
+//console.log(genres);
+
+function getGames() {
 	fetch(
 		`https://api.rawg.io/api/games?key=f4d775e86a54401da676771c802f8d33&ordering=-metacritic&genres=${genres}&platforms=${platforms}&rating=${rating}`
 	)
 		.then((res) => res.json())
-		.then((data) => console.log(data));
-});
+		.then((data) => {
+        for (let i=0; i<data.results.length; i++){
+            let divEl = document.createElement("div");
+            test.append(divEl);
+            divEl.append(data.results[i].name)
+        }
+    })}
+        
+
+
+button.addEventListener('click', function (e) {
+	e.preventDefault();
+	getGames();
+})
