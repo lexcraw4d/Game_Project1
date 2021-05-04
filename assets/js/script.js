36ed22af9b888f23acc7666d10aaaaa8030df929
JOSHUATVN <80295120+JOSHUATVN@users.noreply.github.com>
2021-05-03 20:18:01 -0500

    added local storage for each dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// local storage
+// local storage save favorite
 function saveFavorite() {
     let saveUserData = JSON.parse(localStorage.getItem('')) || [];
     let newSavedData = [{
@@ -15,17 +15,111 @@ function saveFavorite() {
     let newSavedData1 = JSON.parse(localStorage.getItem(""))
 }
 
+
+// localstorage function 
 $(document).ready(function() {
-    $("#---").change(function() {
+    $("#select-box").change(function() {
         localStorage.setItem('genre', $(this).val());
-        $('#---').value(localStorage.getItem('genre'));
+        $('#select-box').value(localStorage.getItem('genre'));
     });
 });
 
+
+
+// genre storage
 if (localStorage.getItem('genre') === null) {
-    localStorage.setItem('genre', "");
-    document.write("option A")
-}
+    localStorage.setItem('genre', "action");
+    document.write("action");
+} else {
+    if (localStorage.getItem('genre') === "action") {
+        document.write("Action");
+    } else if (localStorage.getItem('genre') === "strategy") {
+        document.write("Strategy");
+    } else if (localStorage.getItem('genre') === "sports") {
+        document.write("Sports");
+    } else if (localStorage.getItem('genre') === "rpg") {
+        document.write("RPG");
+    } else if (localStorage.getItem('genre') === "adventure") {
+        document.write("Adventure");
+    } 
+    // add more if we have more selections
+    else if (localStorage.getItem('genre') === "---") {
+        document.write("fillout");
+    } else if (localStorage.getItem('genre') === "---") {
+        document.write("fillout");
+    } else if (localStorage.getItem('genre') === "---") {
+        document.write("fillout");
+    } else if (localStorage.getItem('genre') === "---") {
+        document.write("fillout");
+    } else if (localStorage.getItem('genre') === "---") {
+        document.write("fillout");
+    } 
+};
+
+//console storage
+
+if (localStorage.getItem('console') === null) {
+    localStorage.setItem('console', "4,6");
+    document.write("PC,Web,Linux");
+} else {
+    if (localStorage.getItem('console') === "4,6") {
+        document.write("PC,Web,Linux");
+    } else if (localStorage.getItem('console') === "4,6") {
+        document.write("PC,Web,Linux");
+    } else if (localStorage.getItem('console') === "21,5") {
+        document.write("macOS, Android");
+    } else if (localStorage.getItem('console') === "2") {
+        document.write("Playstation");
+    } else if (localStorage.getItem('console') === "15") {
+        document.write("Playstation 2");
+    } else if (localStorage.getItem('console') === "3") {
+        document.write("Playstation 3");
+    } else if (localStorage.getItem('console') === "4") {
+        document.write("Playstation 4");
+    } else if (localStorage.getItem('console') === "187") {
+        document.write("Playstation 5");
+    } else if (localStorage.getItem('console') === "7") {
+        document.write("Nintendo Switch/Nintendo");
+    } else if (localStorage.getItem('console') === "10,11") {
+        document.write("Wii/Wii U");
+    } else if (localStorage.getItem('console') === "3") {
+        document.write("Xbox");
+    } else if (localStorage.getItem('console') === "1") {
+        document.write("Xbox One");
+    } else if (localStorage.getItem('console') === "186") {
+        document.write("Xbox Series S/X");
+    } 
+};
+
+
+//rating storage
+
+if (localStorage.getItem('rating') === null) {
+    localStorage.setItem('rating', "everyone");
+    document.write("Everyone");
+} else {
+    if (localStorage.getItem('rating') === "everyone") {
+        document.write("Everyone");
+    } else if (localStorage.getItem('rating') === "teen") {
+        document.write("Teen");
+    } else if (localStorage.getItem('rating') === "mature") {
+        document.write("Mature");
+    }
+};
+
+//release date storage
+if (localStorage.getItem('release') === null) {
+    localStorage.setItem('release', "yes");
+    document.write("yes");
+} else {
+    if (localStorage.getItem('release') === "yes") {
+        document.write("Yes");
+    } else if (localStorage.getItem('release') === "no") {
+        document.write("No");
+    }
+};
+
+
 
 
 
